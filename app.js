61ec4392c5108c56fe4435d4c8ee1762c976cd86
Rehab <140162802+rehabkhaleel@users.noreply.github.com>
2023-10-06 19:40:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("JavaScript file is linked externally");
+
+let a = 10; //setting value of a equals to 10
+console.log(a); // printing a
+/*
+this will print the value of a which is currently set to 10
+*/
